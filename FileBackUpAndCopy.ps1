3ce3e6b4112c45c260f69b13b0d3e3930bb8b299
toscan4ik <hingvarson@list.ru>
2021-01-01 01:30:31 +0300

    Script for backup and copy files

---
+++ FileBackUpAndCopy.ps1
@@ -0,0 +1,14 @@
+﻿$Folders = @('TestFolder1', 'TestFolder2', 'TestFolder3', 'TestFolder4', 'TestFolder5')
+$PathToFolders = 'C:\Test\'
+$PathToNewFile = 'C:\Test\'
+$FileName = 'TestFile.txt'
+$BacFileName = $Filename + '.old'
+foreach ($Folder in $Folders){
+    if (Test-Path $PathToFolders\$Folder\$FileName) {
+        Rename-Item -Path $PathToFolders\$Folder\$FileName -NewName $NewFileName
+        Copy-Item $PathToNew\$BacFileName -Destination $PathToFolders\$Folder\
+        }
+    else {
+        Copy-Item $PathToNew\$BacFileName -Destination $PathToFolders\$Folder\
+        }
+    }
